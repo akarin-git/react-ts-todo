f41b058e66b250e0cc4a607d1309f9700560ff09
akarin-git <mwakarin14@gmail.com>
2022-07-02 12:24:40 +0900

    husky 設定調整

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      hi
-
-
-    
-    </div>
-  );
+  return <div className="App">hi</div>;
 }
 
 export default App;
